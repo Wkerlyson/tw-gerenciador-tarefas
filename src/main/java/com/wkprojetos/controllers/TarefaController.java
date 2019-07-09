64b086d7d307f827f67e5c0e3fa5c6fd86693895
wkerlyson Batista <wkerlyson.batista@gmail.com>
2019-07-09 13:46:31 -0300

    Implementação do método inserir; Validação de campos

--- src/main/java/com/wkprojetos/controllers/TarefaController.java
+++ src/main/java/com/wkprojetos/controllers/TarefaController.java
@@ -1,11 +1,18 @@
 package com.wkprojetos.controllers;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wkprojetos.models.Tarefa;
 import com.wkprojetos.repositorys.RepositorioTarefa;
 
 @Controller
@@ -24,7 +31,34 @@ public class TarefaController {
 	}
 	
 	@GetMapping("/inserir")
-	public String inserir() {
-		return "tarefas/inserir";
+	public ModelAndView inserir() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("tarefas/inserir");
+		mv.addObject("tarefa", new Tarefa());
+		return mv;
 	}
+	
+	@PostMapping("/inserir")
+	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		
+		if(tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracao.nula", "A data de expiração é obrigatória");
+		}else {
+			if(tarefa.getDataExpiracao().before(new Date())) {
+				result.rejectValue("dataExpiracao", "tarefa.dataExpiracao.invalida", "A data de expiração não pode ser anterior à data atual");
+			}
+		}
+		
+		if(result.hasErrors()) {
+			mv.setViewName("tarefas/inserir");
+			mv.addObject(tarefa);
+		}else {
+			mv.setViewName("redirect:/tarefas/listar");
+			repositorioTarefa.save(tarefa);
+		}
+		
+		return mv;
+	}
+	
 }
